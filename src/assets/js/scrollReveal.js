0cf8b656d6d2733325eaf8dc85842bafdc7d5994
unknown <maydoug@hotmail.com>
2022-08-05 00:10:05 -0300

    adicionando transicao e ajustes de resposividade

---
+++ src/assets/js/scrollReveal.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({ 
+    reset: true,
+    distance: '200px'
+ })
+
+
+ScrollReveal().reveal('.card-skills', {
+    delay: 200,
+    origin: 'left'
+});
+ScrollReveal().reveal('.form', {
+    delay: 400,
+    origin: 'bottom'
+});
